b62b1b28883c4447906c5d3592efeb0e6c5c60d4
joaoprcoutinho <up202108787@up.pt>
2023-04-16 01:12:59 +0100

    wrong file

--- database.sql
+++ database.sql
@@ -3,63 +3,79 @@
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Client;
 DROP TABLE IF EXISTS Agent;
+DROP TABLE IF EXISTS Admin;
 DROP TABLE IF EXISTS Reply;
 DROP TABLE IF EXISTS Ticket;
 DROP TABLE IF EXISTS TicketLog;
+DROP TABLE IF EXISTS Hashtag;
+DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS Faq;
 
 -- Criar tabelas
 
--- Eliminacao de tabelas anteriores
-
-DROP TABLE IF EXISTS Equipa;
-DROP TABLE IF EXISTS Jornada;
-DROP TABLE IF EXISTS Jogador;
-DROP TABLE IF EXISTS Classificacao;
-DROP TABLE IF EXISTS Jogo;
-DROP TABLE IF EXISTS Golo;
-
--- Criar tabelas
-
-CREATE TABLE Equipa (
-    idEquipa INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome VARCHAR(50) NOT NULL UNIQUE
+CREATE TABLE User (
+    username VARCHAR(25) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(32) NOT NULL UNIQUE
 );
 
-CREATE TABLE Jornada (
-    num INTEGER PRIMARY KEY CHECK(num > 0 AND num <= 34)
+CREATE TABLE Client (
+    username REFERENCES User(username) UNIQUE
 );
 
-CREATE TABLE Jogador (
-    idJogador INTEGER PRIMARY KEY AUTOINCREMENT,
-    num INTEGER NOT NULL CHECK(num > 0 AND num < 100),
-    nome VARCHAR(25) NOT NULL,
-    idEquipa INTEGER REFERENCES Equipa(idEquipa)
+CREATE TABLE Agent (
+    username REFERENCES User(username) UNIQUE
 );
 
-CREATE TABLE Classificacao(
-    idEquipa INTEGER REFERENCES Equipa(idEquipa),
-    numJornada INTEGER REFERENCES Jornada(num) CHECK(numJornada > 0 AND numJornada <= 34),
-    pontos INTEGER NOT NULL CHECK(pontos >= 0),
-    numVitoria INTEGER NOT NULL CHECK(numVitoria >= 0), 
-    numEmpate INTEGER NOT NULL CHECK(numEmpate >= 0),
-    numDerrota INTEGER NOT NULL CHECK(numDerrota >= 0), 
-    diferencaGolos INTEGER NOT NULL,
-    CONSTRAINT pk_equipa_jornada PRIMARY KEY(idEquipa, numJornada)
+CREATE TABLE Admin (
+    username REFERENCES User(username) UNIQUE    
 );
 
-CREATE TABLE Jogo (
-    idJogo INTEGER PRIMARY KEY AUTOINCREMENT,
-    vencedor INTEGER REFERENCES Equipa(idEquipa) CHECK(vencedor = equipaVisitada OR vencedor = equipaVisitante),
-    numJornada INTEGER REFERENCES Jornada(num) CHECK(numJornada > 0 AND numJornada <= 34),
-    equipaVisitada INTEGER REFERENCES Equipa(idEquipa),
-    equipaVisitante INTEGER REFERENCES Equipa(idEquipa)
+CREATE TABLE Department (
+    name VARCHAR(25) PRIMARY KEY,
+    agent REFERENCES Agent(username)
 );
 
-CREATE TABLE Golo (
-    idGolo INTEGER PRIMARY KEY AUTOINCREMENT,
-    minuto INTEGER CHECK(minuto > 0),
-    idJogo INTEGER REFERENCES Jogo(idJogo),
-    equipaMarc INTEGER REFERENCES Equipa(idEquipa),
-    idJogador INTEGER REFERENCES Jogador(idJogador)
+CREATE TABLE Reply (
+    idreply INTEGER PRIMARY KEY,
+    reply VARCHAR, 
+    date INTEGER,
+    attachment VARCHAR,
+    idTicket INTEGER REFERENCES Ticket(idTicket),
+    client REFERENCES Client(username),
+    agent REFERENCES Agent(username),
 );
+
+CREATE TABLE Ticket (
+     idTicket INTEGER PRIMARY KEY AUTOINCREMENT,
+     text VARCHAR,
+     date INTEGER,	
+     status VARCHAR(10),
+     priority INTEGER NOT NULL CHECK(priority >=1),
+     client REFERENCES Client(username),
+     agent REFERENCES Agent(username),
+     department REFERENCES Department(name),
+);
+
+CREATE TABLE Hashtag (
+     hashtag VARCHAR PRIMARY KEY
+)
+
+CREATE TABLE TicketHashtag (
+     hashtag REFERENCES Hashtag(hashtag)
+     idTicket REFERENCES Ticket(idTicket) 
+)
+
+CREATE TABLE Ticketlog (
+     idTicketlog INTEGER PRIMARY KEY,
+     date INTEGER,
+     change VARCHAR,
+     idticket REFERENCES Ticket(idTicket),
+     agente REFERENCES Agent(username)
+)
+
+CREATE TABLE Faq (
+     faq VARCHAR PRIMARY KEY
+)
 

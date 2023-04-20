a12d946ab18b28f5e83ef4f8d58b950e6b2181e0
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2023-04-21 00:17:12 +0100

    added a Session class and a logout option

--- templates/common.php
+++ templates/common.php
@@ -1,18 +1,26 @@
 <?php
   declare (strict_types = 1);
+
+  require_once('/lib/session.php');
 ?>
 
 <?php function outputHeader(): void { ?>
+  <?php $session = new Session(); ?>
   <header>
-    <img src="<?= '/assets/logo.png' ?>" alt="logo" />
-    <h1><a href="<?= '/index.php' ?>">Trouble Ticket Management System</a></h1>
+    <img src="/assets/logo.png" alt="logo" />
+    <h1><a href="/index.php">Trouble Ticket Management System</a></h1>
     <div id="signup">
-      <a href="<?= '/register.php' ?>">Register</a>
-      <a href="<?= '/login.php' ?>">Login</a>
+      <?php if ($session->isLoggedIn()) { ?>
+        <a href="/register.php">Register</a>
+        <a href="/login.php">Login</a>
+      <?php } else { ?>
+        <!-- Add a action to logout -->
+        <a href="#">Logout</a>
+      <?php } ?>
     </div>
     <nav id="menu">
       <ul>
-        <li><a href="<?= '/index.php' ?>">Home</a></li>
+        <li><a href="/index.php">Home</a></li>
         <li><a href="/create_ticket.php">Submit Ticket</a></li>
         <li><a href="/view_ticket.php">View Tickets</a></li>
         <li><a href="#">FAQs</a></li>

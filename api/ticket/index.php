dc9de989f05e93c57ffe308caa242f471e0792e8
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2023-05-06 04:16:58 +0100

    added Create Ticket endpoint in the API (/api/ticket in POST)

--- api/ticket/index.php
+++ api/ticket/index.php
@@ -4,6 +4,7 @@
   require_once(__DIR__ . '/../../lib/http_status.php');
   require_once(__DIR__ . '/../../lib/api.php');
   require_once(__DIR__ . '/../../database/ticket.php');
+  require_once(__DIR__ . '/../../database/user.php');
 
   switch ($_SERVER['REQUEST_METHOD']) {
     case RequestMethod::GET:
@@ -27,6 +28,55 @@
       }
 
       API::sendGetResponse(HttpStatus::OK, $body);
+      return;
+    case RequestMethod::POST: // TODO: Add authentication
+      $json_data = file_get_contents('php://input');
+      $data = json_decode($json_data, true);
+
+      if (!array_key_exists('title', $data) || !array_key_exists('text', $data) || !array_key_exists('client', $data)) {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Missing required fields');
+        return;
+      }
+
+      if (gettype($data['title']) != 'string' || gettype($data['text']) != 'string' || gettype($data['client']) != 'string') {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Invalid field types');
+        return;
+      }
+
+      if (array_key_exists('hashtags', $data) && !is_array($data['hashtags'])) {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Invalid field types');
+        return;
+      }
+
+      if (array_key_exists('department', $data) && gettype($data['department']) != 'string') {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Invalid field types');
+        return;
+      }
+
+      $just_hashtags = array_key_exists('hashtags', $data) && count($data) > 4;
+      $just_department = array_key_exists('department', $data) && count($data) > 4;
+      $both = array_key_exists('hashtags', $data) && array_key_exists('department', $data) && count($data) > 5;
+      if (count($data) > 3 || $just_hashtags || $just_department || $both) {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Too many fields');
+        return;
+      }
+
+      if (!Client::exists($data['client'])) {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Client does not exist');
+        return;
+      }
+
+      if (isset($data['department']) && !Department::exists($data['department'])) {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Department does not exist');
+        return;
+      }
+
+      $hashtags = isset($data['hashtags']) ? $data['hashtags'] : [];
+      $department = isset($data['department']) ? $data['department'] : null;
+
+      Ticket::create($data['title'], $data['text'], new Client($data['client']), $hashtags, $department);
+
+      API::sendPostResponse(HttpStatus::CREATED, ['message' => 'Ticket created']);
       return;
     default:
       API::sendError(HttpStatus::METHOD_NOT_ALLOWED, 'Method not allowed');

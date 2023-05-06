85b1772583a293b879fc28d44d2627e60038ecca
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2023-05-06 15:59:52 +0100

    added login and logout endpoints

---
+++ api/login/index.php
@@ -0,0 +1,45 @@
+<?php
+  declare (strict_types = 1);
+
+  require_once(__DIR__ . '/../../lib/http_status.php');
+  require_once(__DIR__ . '/../../lib/api.php');
+  require_once(__DIR__ . '/../../lib/session.php');
+  require_once(__DIR__ . '/../../database/user.php');
+
+  switch ($_SERVER['REQUEST_METHOD']) {
+    case RequestMethod::POST:
+      $session = new Session();
+      if ($session->isLoggedIn()) {
+        API::sendError(HttpStatus::FORBIDDEN, 'You are already logged in');
+        return;
+      }
+
+      $json_data = file_get_contents('php://input');
+      $data = json_decode($json_data, true);
+
+      if (!array_key_exists('username', $data) || !array_key_exists('password', $data)) {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Missing required fields');
+        return;
+      }
+
+      if (gettype($data['username']) != 'string' || gettype($data['password']) != 'string') {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Invalid field types');
+        return;
+      }
+
+      if (count($data) > 2) {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Too many fields');
+        return;
+      }
+
+      if (User::isValid($data['username'], $data['password'])) {
+        $session->setUser($data['username']);
+        API::sendPostResponse(HttpStatus::OK, ['message' => 'Logged in as ' . $data['username'] . '.']);
+      } else {
+        API::sendError(HttpStatus::UNAUTHORIZED, 'Invalid username or password');
+      }
+      return;
+    default:
+      API::sendError(HttpStatus::METHOD_NOT_ALLOWED, 'Method not allowed');
+  }
+?>
---
+++ api/logout/index.php
@@ -0,0 +1,31 @@
+<?php
+  declare(strict_types=1);
+
+  require_once(__DIR__ . '/../../lib/http_status.php');
+  require_once(__DIR__ . '/../../lib/api.php');
+  require_once(__DIR__ . '/../../lib/session.php');
+  require_once(__DIR__ . '/../../database/user.php');
+
+  switch ($_SERVER['REQUEST_METHOD']) {
+    case RequestMethod::POST:
+      $json_data = file_get_contents('php://input');
+      $data = json_decode($json_data, true);
+
+      if (count($data) > 0) {
+        API::sendError(HttpStatus::BAD_REQUEST, 'Too many fields');
+        return;
+      }
+
+      $session = new Session();
+      if (!$session->isLoggedIn()) {
+        API::sendError(HttpStatus::FORBIDDEN, 'You are not logged in');
+        return;
+      }
+
+      $session->logout();
+      API::sendPostResponse(HttpStatus::OK, ['message' => 'Logged out.']);
+      return;
+    default:
+      API::sendError(HttpStatus::METHOD_NOT_ALLOWED, 'Method not allowed');
+  }
+?>

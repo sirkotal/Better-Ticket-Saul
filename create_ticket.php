82dc737e212aac29d8e19db74961428b12514dff
sirkotal <lordkotal2002@gmail.com>
2023-05-21 18:39:06 +0100

    Prototype page for Ticket submission

--- create_ticket.php
+++ create_ticket.php
@@ -4,22 +4,32 @@
   require_once(__DIR__ . '/templates/common.php');
 ?>
 
-<?php outputHead() ?>
+<?php outputHead(
+  $stylesheets = [
+    '/style/create_ticket.css'
+  ]
+  ) ?>
 <body>
   <?php outputHeader() ?>
   <section id="open">
-    <h1>Open a Ticket</h1>
-    <form>
-      <label>
-        Ticket Title <input type="text" name="title">
-      </label>
-      <label>
-        Add your hashtags <input type="text" name="hashtags">
-      </label>
-      <div class="hash-container"></div>
-      <button formaction="#" formmethod="post">Open</button>
-    </form>
-  </section>
+        <h1>Open a Ticket</h1>
+        <form type="Submit">
+          <label id="title">
+            <input type="text" name="title" placeholder="Add your title">
+          </label>
+          <label id="department">
+            <select name="department">
+              <option value="none" disabled selected hidden>--Select Deparment--</option>
+              <option value="sales">Sales</option>
+              <option value="support">Support</option> 
+              <option value="billing">Billing</option> 
+            </select>
+          </label>
+          <textarea id="hashtag" name="hashtag" placeholder="Add your hashtags"></textarea>
+          <div class="hash-container"></div>
+          <button formaction="#" formmethod="post">Open</button>
+        </form>
+      </section>
   <?php outputFooter() ?>
 </body>
 </html>

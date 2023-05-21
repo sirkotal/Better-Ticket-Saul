3cc71e3a9f10ea27a39bc67d1588c60ba6fe61d9
sirkotal <lordkotal2002@gmail.com>
2023-05-21 16:12:53 +0100

    Changes to both CSS and JavaScript

--- script/departments.js
+++ script/departments.js
@@ -5,10 +5,14 @@ function showAgents() {
     button.addEventListener('click', () => {
       const agentSection = button.parentNode.parentNode;
       const agentData = agentSection.querySelectorAll('.agent-data');
+      const icon = button.querySelector('i');
 
       agentData.forEach((data) => {
         data.classList.toggle('show');
       });
+
+      icon.classList.toggle('fa-caret-square-down');
+      icon.classList.toggle('fa-caret-square-up');
     });
   });
 }
--- script/switch.js
+++ script/switch.js
@@ -1,30 +1,35 @@
 const images = ['/assets/boss1.jpg', '/assets/boss2.jpg', '/assets/boss3.jpg', '/assets/boss4.jpg', '/assets/boss5.jpg', '/assets/boss6.jpg'];
-const container = document.querySelector('.saul-container');
+const container = '.saul-container';
 
-let nextIndex = 1;
+function changeBackgroundImages(images, containerSelector) {
+  const container = document.querySelector(containerSelector);
 
-let preloadImages = [];
+  let nextIndex = 1;
 
-if (container) {
-  container.style.transition = 'background-image 3s ease-in-out';
-  preloadImages = images.map((src) => {
-    const img = new Image();
-    img.src = src;
-    return img;
-  });
+  let preloadImages = [];
+
+  if (container) {
+    container.style.transition = 'background-image 3s ease-in-out';
+    preloadImages = images.map((src) => {
+      const img = new Image();
+      img.src = src;
+      return img;
+    });
+  }
+
+  setInterval(() => {
+    const nextImage = preloadImages[nextIndex];
+    if (nextImage && container) {
+      setTimeout(() => {
+        container.style.backgroundImage = `url(${nextImage.src})`;
+        //container.style.opacity = 1;
+        nextIndex++;
+        if (nextIndex === images.length) {
+          nextIndex = 0;
+        }
+      }, 1000);
+    }
+  }, 5000);
 }
 
-
-setInterval(() => {
-  const nextImage = preloadImages[nextIndex];
-  if (nextImage && container) {
-    setTimeout(() => {
-      container.style.backgroundImage = `url(${nextImage.src})`;
-      //container.style.opacity = 1;
-      nextIndex++;
-      if (nextIndex === images.length) {
-        nextIndex = 0;
-      }
-    }, 1000);
-  }
-}, 5000);
+changeBackgroundImages(images, container);

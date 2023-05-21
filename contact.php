4d3a0246a1f9f77ed90c9a0e94441dc2de9abb4e
sirkotal <lordkotal2002@gmail.com>
2023-05-21 20:17:13 +0100

    Attempting to send an email through the Contact Us form

--- contact.php
+++ contact.php
@@ -34,7 +34,7 @@
       <label>
         <textarea name="message" required></textarea> <p>Message</p> 
       </label>
-      <button formaction="#" formmethod="post">Submit</button>
+      <button formaction="mailto:lordkotal2002@gmail.com" formmethod="post" enctype="text/plain">Submit</button>
     </form>
   </section>
   <?php outputFooter() ?>

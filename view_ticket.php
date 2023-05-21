3db154e6f8a9bf4f9039fa22537f958168fa70db
sirkotal <lordkotal2002@gmail.com>
2023-05-21 18:52:51 +0100

    Prototype page for viewing a Ticket

--- view_ticket.php
+++ view_ticket.php
@@ -4,18 +4,23 @@
   require_once(__DIR__ . '/templates/common.php');
 ?>
 
-<?php outputHead() ?>
+<?php outputHead($stylesheets = [
+    '/style/view_ticket.css'
+  ]
+  ) ?>
 <body>
   <?php outputHeader() ?>
   <section id="ticket"> <!--questions here-->
     <span class="ticketTitle">Do You Smell What The Rock is Cooking</span>
     <span class="ticketName">The Rock</span>
     <span class="ticketDate">2002</span>
-    <span class="ticketPriority">High</span>
-    <span class="ticketHash">#amogus</span>
-    <span class="ticketHash">#fortnite</span>
-    <span class="ticketHash">#faceoff</span>
-    <span class="ticketHash">#tequila</span>
+    <span class="ticketStatus">In progress</span>
+    <ul class="ticketHashtags">
+        <li><a href=#>#amogus</a></li>
+        <li><a href=#>#fortnite</a></li>
+        <li><a href=#>#faceoff</a></li>
+        <li><a href=#>#tequila</a></li>
+    </ul>
   </section>  
   <section id="messages">
     <article class="message">
